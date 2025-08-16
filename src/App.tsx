78c3d98afc168f609a40a9f31194433a1e39fe31
manalan-km <manalananand@gmail.com>
2025-08-16 16:11:51 +1000

    init

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <div className="grid grid-cols-4 grid-rows-4">
+        <p>Hello World</p>
+      </div>
+    </>
+  )
+}
+
+export default App
